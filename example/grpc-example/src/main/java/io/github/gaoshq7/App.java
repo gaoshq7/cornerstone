2a19a3b0c09d830713211b6425693936d0ef7436
Yashiro <yashiro_2022@163.com>
2024-12-19 09:52:13 +0800

    添加rpc

---
+++ example/grpc-example/src/main/java/io/github/gaoshq7/App.java
@@ -0,0 +1,13 @@
+package io.github.gaoshq7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
